1969f774fab5ae3018d6a5c085554101fe0bbabb
nnx-skelet <nnx-skeleton@yandex.ru>
2016-05-06 11:48:46 +0300

    Create Bootstrap.php

---
+++ test/phpunit/Bootstrap.php
@@ -0,0 +1,81 @@
+<?php
+
+namespace CustomerVendorMember\Member\PhpUnit\Test;
+
+use Zend\Loader\AutoloaderFactory;
+use Zend\Loader\StandardAutoloader;
+use RuntimeException;
+
+error_reporting(E_ALL | E_STRICT);
+chdir(__DIR__);
+
+/**
+ * Class Bootstrap
+ *
+ * @package CustomerVendorMember\Member\PhpUnit\Test
+ */
+class Bootstrap
+{
+    /**
+     * Настройка тестов
+     *
+     * @throws \RuntimeException
+     */
+    public static function init()
+    {
+        static::initAutoloader();
+    }
+    /**
+     * Инициализация автозагрузчика
+     *
+     * @return void
+     *
+     * @throws RuntimeException
+     */
+    protected static function initAutoloader()
+    {
+        $vendorPath = static::findParentPath('vendor');
+        if (is_readable($vendorPath . '/autoload.php')) {
+            /** @noinspection PhpIncludeInspection */
+            include $vendorPath . '/autoload.php';
+        }
+        if (!class_exists(AutoloaderFactory::class)) {
+            $errMsg = sprintf('Error init autoloader. Autoloader class %s not exists', AutoloaderFactory::class);
+            throw new RuntimeException($errMsg);
+        }
+        try {
+            AutoloaderFactory::factory([
+                StandardAutoloader::class => [
+                    'autoregister_zf' => true,
+                    'namespaces' => [
+                        'CustomerVendorMember\\Member' => __DIR__ . '/../../src/',
+                        __NAMESPACE__ => __DIR__ . '/tests/',
+                        'CustomerVendorMember\\Member\\PhpUnit\\TestData' => __DIR__ . '/files'
+                    ]
+                ]
+            ]);
+        } catch (\Exception $e) {
+            $errMsg = 'Error init autoloader';
+            throw new RuntimeException($errMsg, $e->getCode(), $e);
+        }
+    }
+    /**
+     * @param $path
+     *
+     * @return bool|string
+     */
+    protected static function findParentPath($path)
+    {
+        $dir = __DIR__;
+        $previousDir = '.';
+        while (!is_dir($dir . '/' . $path)) {
+            $dir = dirname($dir);
+            if ($previousDir === $dir) {
+                return false;
+            }
+            $previousDir = $dir;
+        }
+        return $dir . '/' . $path;
+    }
+}
+Bootstrap::init();
