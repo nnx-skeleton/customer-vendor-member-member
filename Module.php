cefbe268c242f5b2cc84c074e95f507ecfa4a823
dmitry <plitakov@platformasoft.ru>
2016-05-10 14:42:32 +0300

    change config files & composer.json

--- Module.php
+++ Module.php
@@ -4,11 +4,28 @@ namespace CustomerVendorMember\Member;
 
 use Zend\ModuleManager\Feature\AutoloaderProviderInterface;
 use Zend\ModuleManager\Feature\ConfigProviderInterface;
+use Zend\EventManager\EventManagerAwareTrait;
+use Nnx\ModuleOptions\ModuleConfigKeyProviderInterface;
+use Nnx\Module\IntegrationModuleTrait;
+use Nnx\Module\IntegrationModuleInterface;
+use Nnx\Module\CommonModuleOptionsInterface;
+use NnxSkeletonMember\Member\Module as MemberModule;
 
 class Module implements
     AutoloaderProviderInterface,
-    ConfigProviderInterface
+    ConfigProviderInterface,
+    ModuleConfigKeyProviderInterface,
+    IntegrationModuleInterface,
+    CommonModuleOptionsInterface
 {
+    use IntegrationModuleTrait, EventManagerAwareTrait
+    /**
+     * Имя секции в конфигах приложения, содержащая настройки модуля
+     *
+     * @var string
+     */
+    const CONFIG_KEY = 'customer-vendor-member';
+
     /**
      * Имя модуля
      *
@@ -17,11 +34,36 @@ class Module implements
     const MODULE_NAME = __NAMESPACE__;
 
     /**
-     * Имя секции в конфигах приложения, содержащая настройки модуля
+     * Возвращает список модулей, принадлежащих данному сервису
      *
-     * @var string
+     * @return array
      */
-    const CONFIG_KEY = 'customer-vendor-member';
+    public function getServiceModules()
+    {
+        return [
+            MemberModule::MODULE_NAME
+        ];
+    }
+
+    /**
+     * @return string
+     */
+    public function getModuleConfigKey()
+    {
+        return static::CONFIG_KEY;
+    }
+
+    /**
+     * @inheritdoc
+     *
+     * @return array
+     */
+    public function getCommonModuleOptions()
+    {
+        return [
+
+        ];
+    }
 
     /**
      * @return array
